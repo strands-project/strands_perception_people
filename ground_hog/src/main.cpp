9b46ea78cf5c532d5e356ea49fffda28fc96e093
cdondrup <cdondrup@lincoln.ac.uk>
2013-08-11 14:12:34 +0100

    Closing issue #9 and addressing issue #16 Now using a parameter to set the model file path. Added launch file which finds the models in the standard location.

--- ground_hog/src/main.cpp
+++ ground_hog/src/main.cpp
@@ -33,44 +33,44 @@ void render_bbox_2D(strands_perception_people_msgs::GroundHOGDetections& detecti
 
     painter.setPen(pen);
 
-	for(int i = 0; i < detections.pos_x.size(); i++){
-		int x =(int) detections.pos_x[i];
-		int y =(int) detections.pos_y[i];
-		int w =(int) detections.width[i];
-		int h =(int) detections.height[i];
+    for(int i = 0; i < detections.pos_x.size(); i++){
+        int x =(int) detections.pos_x[i];
+        int y =(int) detections.pos_y[i];
+        int w =(int) detections.width[i];
+        int h =(int) detections.height[i];
 
-		painter.drawLine(x,y, x+w,y);
-		painter.drawLine(x,y, x,y+h);
-		painter.drawLine(x+w,y, x+w,y+h);
-		painter.drawLine(x,y+h, x+w,y+h);
+        painter.drawLine(x,y, x+w,y);
+        painter.drawLine(x,y, x,y+h);
+        painter.drawLine(x+w,y, x+w,y+h);
+        painter.drawLine(x,y+h, x+w,y+h);
     }
 }
 
 void messageCallback(const sensor_msgs::Image::ConstPtr &msg)
 {
-  	
-  std::vector<cudaHOG::Detection> detHog;
 
-//  unsigned char image
- QImage image_rgb(&msg->data[0], msg->width, msg->height, QImage::Format_RGB888);
-  int returnPrepare = hog->prepare_image(image_rgb.convertToFormat(QImage::Format_ARGB32).bits(), (short unsigned int) msg->width, (short unsigned int) 	msg->height);
- 
-  if(returnPrepare)
-  {
-     ROS_ERROR("Error by preparing the image");
-     return;
-  }
-  
-   hog->test_image(detHog);
-   hog->release_image();
+    std::vector<cudaHOG::Detection> detHog;
 
-   int w = 64, h = 128;
-   
-   strands_perception_people_msgs::GroundHOGDetections detections;
-  
-   detections.header = msg->header;
-   for(unsigned int i=0;i<detHog.size();i++)
-   {
+    //  unsigned char image
+    QImage image_rgb(&msg->data[0], msg->width, msg->height, QImage::Format_RGB888);
+    int returnPrepare = hog->prepare_image(image_rgb.convertToFormat(QImage::Format_ARGB32).bits(), (short unsigned int) msg->width, (short unsigned int) 	msg->height);
+
+    if(returnPrepare)
+    {
+        ROS_ERROR("Error by preparing the image");
+        return;
+    }
+
+    hog->test_image(detHog);
+    hog->release_image();
+
+    int w = 64, h = 128;
+
+    strands_perception_people_msgs::GroundHOGDetections detections;
+
+    detections.header = msg->header;
+    for(unsigned int i=0;i<detHog.size();i++)
+    {
 
         float scale = detHog[i].scale;
 
@@ -94,9 +94,9 @@ void messageCallback(const sensor_msgs::Image::ConstPtr &msg)
     sensor_image.header = msg->header;
     sensor_image.height = image_rgb.height();
     sensor_image.width  = image_rgb.width();
-	vector<unsigned char> image_bits(image_rgb.bits(), image_rgb.bits()+sensor_image.height*sensor_image.width*3);
-	sensor_image.data = image_bits;
-	sensor_image.encoding = msg->encoding;
+    vector<unsigned char> image_bits(image_rgb.bits(), image_rgb.bits()+sensor_image.height*sensor_image.width*3);
+    sensor_image.data = image_bits;
+    sensor_image.encoding = msg->encoding;
 
     pub_result_image.publish(sensor_image);
     pub_message.publish(detections);
@@ -104,39 +104,42 @@ void messageCallback(const sensor_msgs::Image::ConstPtr &msg)
 
 int main(int argc, char **argv)
 {
-  hog = new  cudaHOG::cudaHOGManager();
-  string conf = "model/config";
-  hog->read_params_file(conf);
-  hog->load_svm_models();
-        
-  // Set up ROS.
-  ros::init(argc, argv, "groundHOG");
-  ros::NodeHandle n;
+    // Set up ROS.
+    ros::init(argc, argv, "groundHOG");
+    ros::NodeHandle n;
 
-  // Declare variables that can be modified by launch file or command line.
-  string topic;
-  string pub_topic;
-  string pub_image_topic;
+    // Declare variables that can be modified by launch file or command line.
+    string topic;
+    string pub_topic;
+    string pub_image_topic;
+    string conf;
 
-  // Initialize node parameters from launch file or command line.
-  // Use a private node handle so that multiple instances of the node can be run simultaneously
-  // while using different parameters.
-  ros::NodeHandle private_node_handle_("~");
-  private_node_handle_.param("topic", topic, string("/camera/rgb/image_color"));
+    // Initialize node parameters from launch file or command line.
+    // Use a private node handle so that multiple instances of the node can be run simultaneously
+    // while using different parameters.
+    ros::NodeHandle private_node_handle_("~");
+    private_node_handle_.getParam("model", conf);
+    private_node_handle_.param("image_color", topic, string("/camera/rgb/image_color"));
+    private_node_handle_.param("detections", pub_topic, string("/groundHOG/detections"));
+    private_node_handle_.param("result_image", pub_image_topic, string("/groundHOG/image"));
 
-  // Create a subscriber.
-  // Name the topic, message queue, callback function with class name, and object containing callback function.
-	//The bigger the queue, the bigger the dealy. 1 is the most real-time.
-  ros::Subscriber sub_message = n.subscribe(topic.c_str(), 1, &messageCallback);
 
-  private_node_handle_.param("topic", pub_topic, string("/groundHOG/detections"));
-  pub_message = n.advertise<strands_perception_people_msgs::GroundHOGDetections>(pub_topic.c_str(), 10);
+    //Initialise cudaHOG
+    hog = new  cudaHOG::cudaHOGManager();
+    hog->read_params_file(conf);
+    hog->load_svm_models();
 
-  private_node_handle_.param("topic", pub_image_topic, string("/groundHOG/image"));
-  pub_result_image = n.advertise<sensor_msgs::Image>(pub_image_topic.c_str(), 10);
-  
-  ros::spin();
-  
-  return 0;
+    // Create a subscriber.
+    // Name the topic, message queue, callback function with class name, and object containing callback function.
+    //The bigger the queue, the bigger the dealy. 1 is the most real-time.
+    ros::Subscriber sub_message = n.subscribe(topic.c_str(), 1, &messageCallback);
+
+    // Create publishers
+    pub_message = n.advertise<strands_perception_people_msgs::GroundHOGDetections>(pub_topic.c_str(), 10);
+    pub_result_image = n.advertise<sensor_msgs::Image>(pub_image_topic.c_str(), 10);
+
+    ros::spin();
+
+    return 0;
 }
 
