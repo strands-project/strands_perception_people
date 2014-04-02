69ea13bdb1d4a39882a18d2253e3d62e9096db03
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-04-02 13:19:19 +0100

    Switched to image_rect_meters topic for depth image

--- strands_visual_odometry/src/main.cpp
+++ strands_visual_odometry/src/main.cpp
@@ -156,9 +156,9 @@ int main(int argc, char **argv)
     private_node_handle_.param("queue_size", queue_size, int(5));
     private_node_handle_.param("camera_namespace", cam_ns, string("/head_xtion"));
 
-    string topic_image_mono = cam_ns + "/rgb/image_rect";
-    string topic_depth_image = cam_ns + "/depth/image_rect";
-    string topic_camera_info = cam_ns + "/rgb/camera_info";
+    string topic_image_mono = cam_ns + "/rgb/image_mono";
+    string topic_depth_image = cam_ns + "/depth/image_rect_meters";
+    string topic_camera_info = cam_ns + "/depth/camera_info";
 
     ROS_DEBUG("visual_odometry: Queue size for synchronisation is set to: %i", queue_size);
 
