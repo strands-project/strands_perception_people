a05096e4706315399f8553391eb3b6e307c2539a
Christian Dondrup <cdondrup@lincoln.ac.uk>
2014-07-14 15:54:10 +0100

    Saving pedestrian tracking and upper body detections in log message.

--- strands_perception_people_utils/scripts/save_locations.py
+++ strands_perception_people_utils/scripts/save_locations.py
@@ -4,6 +4,7 @@ import rospy
 from ros_datacentre.message_store import MessageStoreProxy
 import strands_perception_people_msgs.msg
 import geometry_msgs.msg
+import message_filters
 
 
 class SaveLocations():
@@ -12,12 +13,17 @@ class SaveLocations():
         self.robot_pose = geometry_msgs.msg.Pose()
         self.dataset_name = "locations"
         self.msg_store = MessageStoreProxy(collection="people_perception")
-        rospy.Subscriber(
+        locations = message_filters.Subscriber(
             "/pedestrian_localisation/localisations",
             strands_perception_people_msgs.msg.PedestrianLocations,
-            self.people_callback,
-            None,
-            10
+        )
+        pedestrian = message_filters.Subscriber(
+            "/pedestrian_tracking/pedestrian_array",
+            strands_perception_people_msgs.msg.PedestrianTrackingArray,
+        )
+        upper = message_filters.Subscriber(
+            "/upper_body_detector/detections",
+            strands_perception_people_msgs.msg.UpperBodyDetector,
         )
         rospy.Subscriber(
             "/robot_pose",
@@ -26,8 +32,11 @@ class SaveLocations():
             None,
             10
         )
+        ts = message_filters.TimeSynchronizer([locations, pedestrian, upper], 100)
+        ts.registerCallback(self.people_callback)
 
-    def people_callback(self, pl):
+    def people_callback(self, pl, pt, up):
+        rospy.loginfo("CALLED")
         if len(pl.distances) == 0:
             return
         meta = {}
@@ -42,6 +51,8 @@ class SaveLocations():
         insert.angles = pl.angles
         insert.min_distance = pl.min_distance
         insert.min_distance_angle = pl.min_distance_angle
+        insert.pedestrian_tracking = pt.pedestrians
+        insert.upper_body_detections = up
         self.msg_store.insert(insert, meta)
 
     def pose_callback(self, pose):
