4dc405d6366cf4eb1e1d8ae993de8f739265ac6d
Christian Dondrup <cdondrup@lincoln.ac.uk>
2015-02-20 16:27:25 +0000

    Fixing my sed mistakes and the install targets for human_trajectory.

--- human_trajectory/setup.py
+++ human_trajectory/setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     packages=['human_trajectory'],
-    scripts=['scripts/trajectory_publisher.py'],
+    #scripts=['scripts/trajectory_publisher.py'],
     package_dir={'': 'src'})
 
 setup(**setup_args)
